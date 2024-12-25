07cc23e77ba9b2140d543d67e2ce0a5a6169052f
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2024-12-25 20:04:45 +0000

    :bug:FIX: Replaces ParentClass decoded code to written code

--- instance-blocks/ParentClass.java
+++ instance-blocks/ParentClass.java
@@ -1,6 +1,11 @@
-    public class ParentClass {
-   ParentClass(String var1) {
-      System.out.println("A ParentClass instance is beeing initiated!");
-      System.out.println("ParentClass constructor executed!");
-   }
+public class ParentClass {
+
+    {
+        System.out.println("A ParentClass instance is beeing initiated!");
+    }
+
+    ParentClass(String a) {
+        System.out.println("ParentClass constructor executed!");
+    }
+
 }
